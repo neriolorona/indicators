8c529a88623e523d64604086737ed98ea5c938d4
Nerio Loroña Mayta <neriolorona@MacBook-Pro-de-Nerio.local>
2022-06-01 09:05:56 -0400

    add material ui paper and test in android and ios running

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import {Button, Appbar} from 'react-native-paper';
 
 const App = () => {
   return (
-    <View>
-      <Text>Soy la entrada de la app</Text>
+    <View style={{flex: 1, marginTop: 200}}>
+      <Appbar style={styles.bottom}>
+        <Appbar.Action
+          icon="archive"
+          onPress={() => console.log('Pressed archive')}
+        />
+        <Appbar.Action
+          icon="mail"
+          onPress={() => console.log('Pressed mail')}
+        />
+        <Appbar.Action
+          icon="label"
+          onPress={() => console.log('Pressed label')}
+        />
+        <Appbar.Action
+          icon="delete"
+          onPress={() => console.log('Pressed delete')}
+        />
+      </Appbar>
     </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  bottom: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
